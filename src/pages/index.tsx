03165b97acf81d8eafbb10950ad16ef87640292c
Simon Eich <69088336+SimonEich@users.noreply.github.com>
2023-05-06 13:54:11 +0200

    add auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
 import styles from "./index.module.css";
 import { type NextPage } from "next";
 import Head from "next/head";
@@ -5,6 +6,9 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   
+  const user  = useUser();
+
+
   return (
     <>
       <Head>
@@ -17,34 +21,10 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
+          <div>
+          {!!user.isSignedIn&&<SignInButton/>}
+          {!user.isSignedIn&&<SignUpButton/>}</div>
+          
         </div>
       </main>
     </>
